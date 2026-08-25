e883f5c3cc29ef5acc75f71156745e705142ddb2
rammxyzzi <yannxyz832@gmail.com>
2026-08-25 18:33:33 +0700

    Update Supabase URL to remove endpoint path

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://awbklgvwnsofkaiwjcso.supabase.co/rest/v1/';
+const supabaseUrl = 'https://awbklgvwnsofkaiwjcso.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3YmtsZ3Z3bnNvZmthaXdqY3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2NTE1MTEsImV4cCI6MjEwMzIyNzUxMX0.Cli1QGWqU7TrNEgUnPxmcUTdt8P5uR60ppqzIdTrt8Q';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
